d8e2bd1b67c288ba6317f5819ffdbf4ad19ee72d
amsterdam <amsterdam@amsterdam.amsterdam>
2023-05-27 00:19:56 -0500

    practica sobre el uso de JBDC

--- practica JDBC/src/jdbc/Cliente.java
+++ practica JDBC/src/jdbc/Cliente.java
@@ -1,34 +1,52 @@
 package jdbc;
 
-import org.h2.Driver;
-
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.Statement;
-
-import static org.h2.util.JdbcUtils.getConnection;
+import java.sql.*;
 
 public class Cliente {
-    public static void main(String[] args){
-        //conectamos con la DB por primera vez
+    public static void main(String[] args) {
+        //Por buena practica, evitamos que la DB se inicie por defecto:
         Connection connection = null;
         try {
+            //Generamos la conexion:
             connection = getConnection();
-            //preparamos la TABLA
+            //Preparamos la tabla:
             Statement statement = connection.createStatement();
-            //borramos la tabla si hay alguna existentes y creamos una nueva tabla en caso qeu no lo haya. Esto lo lograremos mediante el usso de "Statement"
-            statement.execute("DROP TABLE IF EXISTS ANIMALES" +
-                    "CREATE TABLE ANIMALES(ID INT PRYMARI KEY, NOMBRE VARCHAR (100) NOT NULL, TIPO VARCHAR(10)NOT NULL)");
-        }catch (Exception e){
+            //Borramos la tabla si hay alguna existentes y creamos una nueva tabla en caso que no la haya. Esto lo lograremos mediante el usso de "Statement":
+            statement.execute("DROP TABLE IF EXISTS ANIMALES;" +
+                    "CREATE TABLE ANIMALES(ID INT PRIMARY KEY, NOMBRE VARCHAR (100) NOT NULL, TIPO VARCHAR(10)NOT NULL);");
+            System.out.println("Se ha creado la tabla: ANIMALES");
+            //Insertamos los datos de 05 animales:
+            statement.execute("INSERT INTO ANIMALES VALUES(1,'JIRAFA','MAMIFERO'),(2,'AGUILA','AVE'),(3,'PULPO','MOLUSCO'),(4,'IGUANA','REPTIL'),(5,'PERRO','MAMIFERO');");
+            System.out.println("Se han insertado 05 animales a la tabla");
+            //Mostramos la tabla. Para ello agregamos ResultSet:
+            ResultSet showAdd = statement.executeQuery("SELECT * FROM ANIMALES;");
+            //mostramos por pantalla:
+            while (showAdd.next()) {
+                System.out.println("Nombre: " + showAdd.getString(2) + " - Tipo: " + showAdd.getString(3));
+            }
+            System.out.println("Se ha mostrado el contenido de la tabla ANIMALES");
+            System.out.println("============================================================================================ ");
+            //Borramos un animal y mostramos de nuevo la tabla:
+            statement.execute("DELETE FROM ANIMALES WHERE ID = 1;");
+            System.out.println("Se ha eliminado 01 animal de la tabla");
+            ResultSet showDelete = statement.executeQuery("SELECT * FROM ANIMALES;");
+            //mostramos por pantalla:
+            while (showDelete.next()) {
+                System.out.println("Nombre: " + showDelete.getString(2) + " - Tipo: " + showDelete.getString(3));
+            }
+            System.out.println("fin de la ejecucion");
+
+
+        } catch (Exception e) {
             e.printStackTrace();
         }
+    }
 
     //ESTABLECEMOS LA CONEXION:
-    public static Connection getConnection(){
+    public static Connection getConnection() throws Exception {
         Class.forName("org.h2.Driver");
-        return DriverManager.getConnection("jdbc:h2:~/baseDedatos", "root", "root");
+        return DriverManager.getConnection("jdbc:h2:~/github/EJERCICIOS_EN_BACKEND/practica JDBC/src/jdbc]", "root", "toor");
     }
 }
 
-    private static Connection getConnection() {
-    }
+
