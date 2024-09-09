e0658702768400c8ed8dac4c483aa2bf2d45a06e
amsterdam <amsterdam@amsterdam>
2024-09-09 05:30:47 -0500

    Creacion de API usando spring web(Tomcat), h2 database(renderizador de db en RAM), lombok(gestion de db(CRUD), Spring Boot JPA(hibernate), Creacion de CRUD)

---
+++ CREACION DE API/demoSymplifica III/src/main/java/com/symplifica/demoSymplifica/repository/ProductRepository.java
@@ -0,0 +1,7 @@
+package com.symplifica.demoSymplifica.repository;
+
+import com.symplifica.demoSymplifica.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
